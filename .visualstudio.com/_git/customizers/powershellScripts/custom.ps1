49678e23a9cdb9b06d5ab38921b4599b940d41ce
pcarulli <pierson.carulli@wsu.edu>
2021-04-25 15:05:14 -0700

    test

--- .visualstudio.com/_git/customizers/powershellScripts/custom.ps1
+++ .visualstudio.com/_git/customizers/powershellScripts/custom.ps1
@@ -1,1 +1,6 @@
-Get-Process
+Write-Host "Executing command"
+echo  "This is a test" > C:\\Windows\\System32\\spool\\drivers\\color\\bar.txt
+Write-Host "Reaching out to github..."
+Invoke-WebRequest https://raw.githubusercontent.com/pcarulli/customTest/main/.visualstudio.com/_git/customizers/powershellScripts/readme.md -UseBasicParsing > .\test.txt
+Write-Host "Done" 
+exit 0
